9c321d383176b3f203db8eed12f90c2ef287366b
Matthew Rizzini <matthewrizzini@gmail.com>
2018-01-20 17:43:28 +0000

    Completed Express Level 1 Assignment

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ app.get('/', function(request, response){
 });
 
 app.get('/name', function(request, response){
-    response.send('Matt Rizzini');
+    var name = "Matt Rizzini"
+    response.send("Hello, my name is " + name);
 });
 
 app.get('/redirect', function(request, response){
     response.redirect(301, '/surprise');
 });
 
+app.get('/surprise', function(request, response){
+    response.send("Surprise! Your are now here instead of /redirect");
+});
+
+app.get('/date', function(request, response){
+    response.send("Currently, it is " + new Date());
+});
 
 app.listen(process.env.PORT), function(){
     console.log("listening!!");
