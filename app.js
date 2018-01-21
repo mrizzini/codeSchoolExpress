63936ee4e5e9b3c3b0b9d8e4b5dcc42859eb4725
Matthew Rizzini <matthewrizzini@gmail.com>
2018-01-21 03:58:15 +0000

    Completed Express Level 3 Assignment

--- app.js
+++ app.js
@@ -15,16 +15,60 @@ app.get('/redirect', function(request, response){
 });
 
 app.get('/surprise', function(request, response){
-    response.send("Surprise! Your are now here instead of /redirect");
+    response.send("Surprise! You are now here instead of /redirect");
 });
 
 app.get('/date', function(request, response){
     response.send("Currently, it is " + new Date());
 });
 
+var cities = {
+    "Providence": "Rhode Island", 
+    "Portland": "Maine", 
+    "Boston": "Massachusetts", 
+    "Miami": "Florida", 
+    "Newport": "Rhode Island"
+};
+
+
+app.param('name', function(request, response, next){
+    var name = request.params.name;
+    var city = name[0].toLocaleUpperCase() + name.slice(1).toLowerCase();
+    request.cityName = city;
+    next();
+});
+    
+
+app.get('/cities/:name', function (request, response){
+    // var name = request.params.name;
+    // var city = name[0].toLocaleUpperCase() + name.slice(1).toLowerCase();
+    // var states = cities[city];
+    var states = cities[request.cityName];
+    
+    if (!states) {
+        response.status(404).json("No city found for " + request.params.name);
+    } else {
+        response.json(states);
+    }
+    //   if (request.query.limit > 0 && request.query.limit < cities.length) {
+    //     response.json(cities.slice(0, request.query.limit));
+    // } else if (request.query.limit > cities.length) {
+    //     response.status(404).json("Limit must be " + cities.length + " or less");
+    // } else {
+    //     response.json(cities); 
+    // }
+});
+
+
 app.get('/cities', function (request, response){
-    var cities = ["Providence", "Portland", "Boston", "New York", "Newport"];
-    response.json(cities); 
+
+    if (request.query.limit > 0 && request.query.limit < cities.length) {
+        response.json(cities.slice(0, request.query.limit));
+    } else if (request.query.limit > cities.length) {
+        response.status(404).json("Limit must be " + cities.length + " or less");
+    } else {
+        response.json(cities); 
+    }
 });
 
 // app.get('/', function(request, response){
