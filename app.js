9047fd855908b296e4500bd540e8329cb5932fdc
Matthew Rizzini <matthewrizzini@gmail.com>
2018-01-22 04:02:58 +0000

    Completed Express Level 4 Assignment

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+
 
 // app.get('/', function(request, response){
 //     response.send("Hello World"); 
 // });
-
 app.get('/name', function(request, response){
     var name = "Matt Rizzini";
     response.send("Hello, my name is " + name);
 });
-
 app.get('/redirect', function(request, response){
     response.redirect(301, '/surprise');
 });
-
 app.get('/surprise', function(request, response){
     response.send("Surprise! You are now here instead of /redirect");
 });
-
 app.get('/date', function(request, response){
     response.send("Currently, it is " + new Date());
 });
@@ -27,28 +26,30 @@ var cities = {
     "Portland": "Maine", 
     "Boston": "Massachusetts", 
     "Miami": "Florida", 
-    "Newport": "Rhode Island"
+    "Newport": "Rhode Island",
+    "Burlington": "Vermont"
 };
 
-
 app.param('name', function(request, response, next){
-    var name = request.params.name;
-    var city = name[0].toLocaleUpperCase() + name.slice(1).toLowerCase();
-    request.cityName = city;
+    // var name = request.params.name;
+    // var city = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    // request.cityName = city;
+    request.cityName = parseCityName(request.params.name);
     next();
 });
     
-
 app.get('/cities/:name', function (request, response){
     // var name = request.params.name;
     // var city = name[0].toLocaleUpperCase() + name.slice(1).toLowerCase();
     // var states = cities[city];
+    var cityName = parseCityName(request.params.name);
+    var cityInfo = cities[cityName];
     var states = cities[request.cityName];
     
-    if (!states) {
-        response.status(404).json("No city found for " + request.params.name);
+    if (cityInfo) {
+        response.json(cityInfo);
     } else {
-        response.json(states);
+        response.status(404).json("No city found for " + cityName);
     }
     //   if (request.query.limit > 0 && request.query.limit < cities.length) {
     //     response.json(cities.slice(0, request.query.limit));
@@ -59,16 +60,44 @@ app.get('/cities/:name', function (request, response){
     // }
 });
 
+function parseCityName(name) {
+  var parsedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+  return parsedName;
+}
 
 app.get('/cities', function (request, response){
-
+    
     if (request.query.limit > 0 && request.query.limit < cities.length) {
         response.json(cities.slice(0, request.query.limit));
     } else if (request.query.limit > cities.length) {
         response.status(404).json("Limit must be " + cities.length + " or less");
     } else {
-        response.json(cities); 
+        response.json(Object.keys(cities)); 
     }
+});
+
+app.post('/cities', parseUrlencoded, function(request, response){
+    if (request.body.city.length >= 4 && request.body.state.length >= 2) {
+    var city = createCity(request.body.city, request.body.state);
+    console.log("city is " + request.body.city);
+    console.log("state is " + request.body.state);
+    response.status(201).json(city);
+    } else {
+        response.status(400).json('City must be at least 4 characters long and state must be at least 2');
+    }
+    // var newCity = request.body;
+    // cities[newCity.city] = newCity.state;
+    // response.status(201).json(newCity.city);
+});
+
+var createCity = function(name, description){
+  cities[name] = description;
+  return name; 
+};
+
+app.delete('/cities/:name', function(request, response){
+   delete cities[request.cityName];
+   response.sendStatus(200);
 });
 
 // app.get('/', function(request, response){
--- public/client.js
+++ public/client.js
@@ -1,25 +1,52 @@
-/*global $ */
-
-$(document).ready(function() {
-
+/*global $ cities */
 console.log("cities test");
-
-$(function (){
-    console.log("cities test2");
-    $.get('/cities', appendToOptions); 
-        console.log("cities test3");
-
-        function appendToOptions(cities) {
-            var options = [];
-            for (var i in cities) {
-                options.push($('<option>', {text: cities[i]}));
-            }
-            $('#cityOptions').append(options);
-        }
+var cityList = [];
+$(function() {
     
-});
+	$.get('/cities', appendToOptions);
+	console.log("cities test2");
 
-
-
-
+	function appendToOptions(cities) {
+		var list = [];
+		for (var i in cities) {
+			cityList = cities[i];
+			var content = '<a href="/cities/' + cityList + '">' + cityList + '</a> ' + '<a href="#" data-block="' + cityList + '"><img src="https://www.engelvoelkers.com/webinnovation-frontend-resources/images/X_small_red.svg"></a>';
+			list.push($('<li>', {
+				html: content
+			}));
+			// list.push($('<li>', {text: cities[i]}));
+		}
+		$('#city-list').append(list);
+	}
+	
+	$("#formSubmit").on('submit', function(event) {
+		console.log("submit test");
+		event.preventDefault();
+		var form = $(this);
+		var cityData = form.serialize();
+		console.log(cityData);
+		console.log(cityList);
+		$.ajax({
+			type: 'POST',
+			url: '/cities',
+			data: cityData
+		}).done(function(cityName) {
+			console.log(cityName);
+			appendToOptions([cityName]);
+			form.trigger('reset');
+		});
+	});
+	
+	$("#city-list").on('click', 'a[data-block]', function(event) {
+		var target = $(event.currentTarget);
+		if (!confirm('Are you sure you want to delete ' + target.data('block') + "?")) {
+			return false;
+		}
+		$.ajax({
+			type: 'DELETE',
+			url: '/cities/' + target.data('block')
+		}).done(function() {
+			target.parents('li').remove();
+		});
+	});
 });
